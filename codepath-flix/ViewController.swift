b01d80a53635f2b388bb29b450318120f18ef0a1
Ethan Cooper <cravenwarrior3@gmail.com>
2022-09-12 21:35:30 -0400

    Initial Commit

---
+++ codepath-flix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  codepath-flix
+//
+//  Created by Ethan Cooper on 9/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ codepath-flixUITests/codepath_flixUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  codepath_flixUITestsLaunchTests.swift
+//  codepath-flixUITests
+//
+//  Created by Ethan Cooper on 9/12/22.
+//
+
+import XCTest
+
+class codepath_flixUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
