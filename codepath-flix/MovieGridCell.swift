6b27cd6d9f87ead8e9787dd735dc7173c9a1e7de
Ethan Cooper <cravenwarrior3@gmail.com>
2022-09-19 23:15:04 -0400

    Part 2

---
+++ codepath-flix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  codepath-flix
+//
+//  Created by Ethan Cooper on 9/19/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
